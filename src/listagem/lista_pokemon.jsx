f704d0f340fff97c98beadbf3e060a444dc68efe
layumitmatos <layumi.matos@gmail.com>
2024-05-06 08:28:09 -0300

    pokemon listagem

---
+++ src/listagem/lista_pokemon.jsx
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react'
+
+const Listagem = () => (
+    <div> 
+        <ul> 
+            <list>Pokemon l</list>
+            <list>Pokemon 2</list>
+            <list>Pokemon 3</list>
+            <list>Pokemon 4</list>
+            <list>Pokemon 5</list>
+        </ul>
+    </div>
+)
+
+export default Listagem 
